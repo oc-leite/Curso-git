81840fb06f092ff99c840ba6aa7a0cee12cc31c2
Octávio Leite <octaviomartins@live.com>
2022-08-26 11:07:59 -0300

    adicionado mensagens

--- Git_Github/Program.cs
+++ Git_Github/Program.cs
@@ -8,6 +8,8 @@ namespace Git_Github {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá MUndo, Bom dia");
+            Console.WriteLine("Como você esta?");
+            Console.WriteLine("espero que bem");
         }
     }
 }
