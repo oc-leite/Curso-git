8819609e36b1a5db5104e3bc1afbd91d7eabf781
Octávio Leite <octaviomartins@live.com>
2022-08-26 10:46:08 -0300

    acrescentada a mensagem de olá mundo

--- Git_Github/Program.cs
+++ Git_Github/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Git_Github {
     internal class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Olá MUndo, Bom dia");
         }
     }
 }
